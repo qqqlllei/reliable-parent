603ef8409f5d6d388728e13a211d999f5b1ddbc3
lilei <861971291@qq.com>
2018-10-16 15:25:41 +0800

    server 修改

--- reliable-server/src/main/java/com/reliable/message/server/controller/MessageController.java
+++ reliable-server/src/main/java/com/reliable/message/server/controller/MessageController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/message")
-public class MqMessageController {
+public class MessageController {
 
     @Autowired
     private MessageService messageService;
